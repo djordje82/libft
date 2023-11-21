2ba1ec4c1e607a4767e1d391f785ce5f5d80cda8
djordje82 <127096833+djordje82@users.noreply.github.com>
2023-11-21 10:44:41 +0100

    strrchr update

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ddordevi <ddordevi@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:03:07 by dodordev          #+#    #+#             */
-/*   Updated: 2023/11/17 17:15:22 by ddordevi         ###   ########.fr       */
+/*   Updated: 2023/11/20 10:55:07 by ddordevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int			i;
-	const char	*ini;
+	unsigned int	i;
+	unsigned char	uc;
 
-	ini = s;
+	uc = (unsigned char)c;
 	i = ft_strlen(s);
-	s = (s + i);
-	while (*s != *ini && c != *s)
-		s--;
-	if (c == *s)
-		return ((char *)s);
-	return (0);
-}
-/*
-char	*ft_strrchar_v2(const char *s, int c)
-{
-	int	len;
-
-	len = ft_strlen(s);
-	while (len >= 0)
+	while (i + 1)
 	{
-		if (*(s + len) == c)
-			return ((char *)(s + len));
-		len--;
+		if (s[i] == uc)
+			return ((char *)&s[i]);
+		i--;
 	}
-	return (NULL);
-}
-
-int main()
-{
-	const char *test_string = "Hello, world!";
-	int test_char = 'o';
-
-	char *result_custom = ft_strrchr(test_string, test_char);
-	//char *result_customv2 = ft_strrchar_v2(test_string, test_char);
-	char *result_standard = strrchr(test_string, test_char);
-
-	printf("Custom function result: %s\n", result_custom);
-	//printf("Custom function v2 result: %s\n", result_customv2);
-	printf("Standard function result: %s\n", result_standard);
-
 	return (0);
 }
-*/
 
 
