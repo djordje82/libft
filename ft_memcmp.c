0398e49128e8cb75faf2df20aa4870b3410ec7ec
Dorde Dordevic <dodordev@c0c1c2.42berlin.de>
2023-11-21 12:03:01 +0100

    norminette_fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,10 +6,23 @@
 /*   By: dodordev <dodordev@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:46:12 by dodordev          #+#    #+#             */
-/*   Updated: 2023/11/21 11:15:23 by dodordev         ###   ########.fr       */
+/*   Updated: 2023/11/21 11:51:06 by dodordev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	const unsigned char	*s1p;
+	const unsigned char	*s2p;
 
+	s1p = s1;
+	s2p = s2;
+	while (n-- < 0)
+	{
+		if (*s1p != *s2p)
+			return (*s1p - *s2p);
+		s1p++;
+		s2p++;
+	}
+	return (0);
+}
