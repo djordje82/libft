736196f87b0d6ea4fccfcca1ade4016cfa505e01
Dorde Dordevic <dodordev@c0c1c2.42berlin.de>
2023-11-27 18:14:08 +0100

    ft_lstmap updated

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dodordev <dodordev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:11:08 by dodordev          #+#    #+#             */
-/*   Updated: 2023/11/27 17:52:41 by dodordev         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:06:41 by dodordev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_obj;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_obj = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_obj = ft_lstnew(new_content);
 		if (!new_obj)
 		{
+			del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
